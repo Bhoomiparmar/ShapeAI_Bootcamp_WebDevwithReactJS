03c3b2c913c47e1ca6fe433fc3eade630ef480ab
Bhoomiparmar <68984492+Bhoomiparmar@users.noreply.github.com>
2021-06-27 14:06:02 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and Node.js bootcamp</h1>
+      <p>A Basic Backend With Node.js Bootcamp</p>
+    </div>
+  );
+}
+
+export default Info;
